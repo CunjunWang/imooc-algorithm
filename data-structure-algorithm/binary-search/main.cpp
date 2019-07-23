8a80391ce26bd865cfbfa0f835ade0812f0a79b0
Cunjun Wang <13621691063@163.com>
2019-07-23 22:04:06 +0800

    insert in bst

--- data-structure-algorithm/binary-search/main.cpp
+++ data-structure-algorithm/binary-search/main.cpp
@@ -40,6 +40,29 @@ public:
         return count == 0;
     }
 
+    void insert(Key key, Value value) {
+        root = insert(root, key, value);
+    }
+
+private:
+
+    // 向以node为根的bst插入
+    Node *insert(Node *node, Key key, Value value) {
+        if (node == NULL) {
+            count++;
+            return new Node(key, value);
+        }
+        if (key == node->key) {
+            node->value = value;
+        } else if (key < node->key) {
+            node->left = insert(node->left, key, value);
+        } else {
+            node->right = insert(node->right, key, value);
+        }
+
+        return node;
+    }
+
 };
 
 int main() {
