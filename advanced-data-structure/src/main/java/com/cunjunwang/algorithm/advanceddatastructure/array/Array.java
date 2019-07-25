365e81f49d8930f0b2512fc5356d9542040c8f6f
Cunjun Wang <13621691063@163.com>
2019-07-25 13:55:39 +0800

    insert to adday

--- advanced-data-structure/src/main/java/com/cunjunwang/algorithm/advanceddatastructure/array/Array.java
+++ advanced-data-structure/src/main/java/com/cunjunwang/algorithm/advanceddatastructure/array/Array.java
@@ -55,4 +55,49 @@ public class Array {
         return this.size == 0;
     }
 
+    /**
+     * 向数组最后添加元素e
+     *
+     * @param e 添加元素
+     */
+    public void addLast(int e) {
+//        if (size == data.length) {
+//            throw new IllegalArgumentException("AddLast failed, array is already full");
+//        }
+//        // data[size++] = e;
+//        data[size] = e;
+//        size++;
+        add(size, e);
+    }
+
+    /**
+     * 向数组头添加元素e
+     *
+     * @param e 添加元素
+     */
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+
+    /**
+     * 向指定位置index最后添加元素e
+     *
+     * @param index 位置
+     * @param e     添加元素
+     */
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("AddLast failed, array is already full");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("AddLast failed, Require index >= 0 and index <= size");
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
 }
