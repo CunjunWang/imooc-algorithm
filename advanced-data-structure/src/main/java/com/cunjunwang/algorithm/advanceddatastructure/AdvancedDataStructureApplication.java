7a4a3ff6018da0b5e9527e85c2fbbfe1a6aefe2b
Cunjun Wang <13621691063@163.com>
2019-07-25 13:36:48 +0800

    构建java项目

---
+++ advanced-data-structure/src/main/java/com/cunjunwang/algorithm/advanceddatastructure/AdvancedDataStructureApplication.java
@@ -0,0 +1,13 @@
+package com.cunjunwang.algorithm.advanceddatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedDataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedDataStructureApplication.class, args);
+    }
+
+}
