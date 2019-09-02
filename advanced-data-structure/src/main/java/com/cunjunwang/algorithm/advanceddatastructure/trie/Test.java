e30ce0f38b6b160e8e81fa90f28daeafbcb0ccc9
Cunjun Wang <13621691063@163.com>
2019-09-02 16:54:25 -0400

    build a trie and add a word to trie

---
+++ advanced-data-structure/src/main/java/com/cunjunwang/algorithm/advanceddatastructure/trie/Test.java
@@ -0,0 +1,7 @@
+package com.cunjunwang.algorithm.advanceddatastructure.trie;
+
+/**
+ * Created by CunjunWang on 2019-09-02.
+ */
+public class Test {
+}
---
+++ advanced-data-structure/src/main/java/com/cunjunwang/algorithm/advanceddatastructure/trie/Trie.java
@@ -0,0 +1,62 @@
+package com.cunjunwang.algorithm.advanceddatastructure.trie;
+
+import java.util.TreeMap;
+
+/**
+ * Created by CunjunWang on 2019-09-02.
+ */
+public class Trie {
+
+    private class Node {
+        public boolean isWord; // 当前节点是否是一个单词的结尾
+        public TreeMap<Character, Node> next; // 后续节点集合
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+
+    private Node root; // Trie的根节点
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    /**
+     * 获取Trie存储了多少个单词
+     *
+     * @return size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 向trie中添加新的单词
+     *
+     * @param word 待添加的单词
+     */
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        // 把当前节点标记为单词的结尾, 且这个节点之前不能是一个单词
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+}
