96c0c80eb067ac4cbb4c2d127c939abda87a119c
Cunjun Wang <duckwcj@gmail.com>
2019-11-17 13:45:30 -0500

    bellman-ford implementation

---
+++ advanced-graph/src/main/java/com/cunjunwang/algorithm/advancedgraph/weighted_shortest_path/BellmanFord.java
@@ -0,0 +1,85 @@
+package com.cunjunwang.algorithm.advancedgraph.weighted_shortest_path;
+
+import com.cunjunwang.algorithm.advancedgraph.WeightedGraph;
+
+import java.util.Arrays;
+
+/**
+ * Created by CunjunWang on 2019-11-17.
+ */
+public class BellmanFord {
+    private WeightedGraph G;
+
+    private int s;
+
+    private int[] dis;
+
+    private boolean hasNegativeCycle;
+
+    /*
+     * 进行V-1轮松弛操作
+     * 每次松弛操作要遍历每一条边
+     *
+     * O(V*E)
+     */
+    public BellmanFord(WeightedGraph G, int s) {
+        this.G = G;
+        G.validateVertex(s);
+        this.s = s;
+
+        dis = new int[G.V()];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[s] = 0;
+
+        hasNegativeCycle = false;
+
+        // V - 1 次 relaxation
+        for (int pass = 1; pass < G.V(); pass++) {
+
+            for (int v = 0; v < G.V(); v++)
+                for (int w : G.adj(v))
+                    if (dis[v] != Integer.MAX_VALUE &&
+                            dis[v] + G.getWeight(v, w) < dis[w])
+                        dis[w] = dis[v] + G.getWeight(v, w);
+
+        }
+
+        // 再多进行一轮松弛，如果发现还能更新，说明已经有负权环了
+        for (int v = 0; v < G.V(); v++)
+            for (int w : G.adj(v))
+                if (dis[v] != Integer.MAX_VALUE &&
+                        dis[v] + G.getWeight(v, w) < dis[w])
+                    hasNegativeCycle = true;
+
+    }
+
+    public boolean isHasNegativeCycle() {
+        return hasNegativeCycle;
+    }
+
+    public boolean isConnectedTo(int v) {
+        G.validateVertex(v);
+        return dis[v] != Integer.MAX_VALUE;
+    }
+
+    public int distTo(int v) {
+        G.validateVertex(v);
+        if (hasNegativeCycle)
+            throw new RuntimeException("Has negative cycle");
+        return dis[v];
+    }
+
+    public static void main(String[] args) {
+        WeightedGraph g = new WeightedGraph("./src/main/java/com/cunjunwang/algorithm/advancedgraph/test_graph/bf_g.txt");
+        BellmanFord bellmanFord = new BellmanFord(g, 0);
+
+        if (!bellmanFord.hasNegativeCycle) {
+            for (int v = 0; v < g.V(); v++)
+                System.out.print(bellmanFord.distTo(v) + " ");
+            System.out.println();
+        } else {
+            System.out.println("Has negative cycle");
+        }
+
+    }
+}
